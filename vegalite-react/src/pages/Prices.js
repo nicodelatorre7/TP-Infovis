2a1f4e034d7aa50ebf2d0933e070241a8c5c6b94
ndelatorre <ndelatorre@itba.edu.ar>
2022-11-07 09:32:49 -0300

    remove comments

--- vegalite-react/src/pages/Prices.js
+++ vegalite-react/src/pages/Prices.js
@@ -3,8 +3,6 @@ import { Container } from "reactstrap";
 import { Button } from "react-bootstrap";
 import StateBar from "../components/Mode2Charts/StateBar.js";
 import CompanyBar from "../components/Mode2Charts/CompanyBar.js";
-// import PricesInteractive from "../components/Mode2Charts/PricesInteractive.js";
-// import Test from "../components/Mode2Charts/Test.js";
 
 const Prices = () => {
   const [chart, setChart] = useState("");
@@ -22,20 +20,12 @@ const Prices = () => {
             <Button variant="primary" size="lg" active onClick={() => setChart("CompanyBar")}>
               Empresa bandera
             </Button>{" "}
-            {/* <Button variant="primary" size="lg" active onClick={() => setChart("PricesInteractive")}>
-              Precios
-            </Button>{" "}
-            <Button variant="primary" size="lg" active onClick={() => setChart("TEST")}>
-              TEST
-            </Button>{" "} */}
           </>
         </Container>
         <br />
         <br />
         {chart === "StateStationsBar" ? <StateBar /> : <></>}
         {chart === "CompanyBar" ? <CompanyBar /> : <></>}
-        {/* {chart === "PricesInteractive" ? <PricesInteractive /> : <></>}
-        {chart === "TEST" ? <Test /> : <></>} */}
       </div>
     </React.Fragment>
   );
